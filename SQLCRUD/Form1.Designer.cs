a0be5aa1758e0909927a129b64a5b5ee1fa60f8a
Rhan Barredo <rhanbarredo@gamil.com>
2018-03-13 14:29:30 +0800

    Add MD File

---
+++ SQLCRUD/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace SQLCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_save = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.txtbox_title = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbox_author = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbox_decription = new System.Windows.Forms.TextBox();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(65, 217);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(75, 23);
+            this.btn_save.TabIndex = 0;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TITLE";
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(339, 34);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.Size = new System.Drawing.Size(774, 383);
+            this.dgv.TabIndex = 2;
+            this.dgv.DoubleClick += new System.EventHandler(this.dgv_DoubleClick);
+            // 
+            // txtbox_title
+            // 
+            this.txtbox_title.Location = new System.Drawing.Point(130, 34);
+            this.txtbox_title.Name = "txtbox_title";
+            this.txtbox_title.Size = new System.Drawing.Size(183, 22);
+            this.txtbox_title.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "AUTHOR";
+            // 
+            // txtbox_author
+            // 
+            this.txtbox_author.Location = new System.Drawing.Point(130, 77);
+            this.txtbox_author.Name = "txtbox_author";
+            this.txtbox_author.Size = new System.Drawing.Size(183, 22);
+            this.txtbox_author.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "DESCRIPTION";
+            // 
+            // txtbox_decription
+            // 
+            this.txtbox_decription.Location = new System.Drawing.Point(130, 119);
+            this.txtbox_decription.Multiline = true;
+            this.txtbox_decription.Name = "txtbox_decription";
+            this.txtbox_decription.Size = new System.Drawing.Size(183, 78);
+            this.txtbox_decription.TabIndex = 3;
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(153, 217);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_delete.TabIndex = 0;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(238, 217);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_clear.TabIndex = 0;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1176, 447);
+            this.Controls.Add(this.txtbox_decription);
+            this.Controls.Add(this.txtbox_author);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtbox_title);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_save);
+            this.Name = "Form1";
+            this.Text = "BOOK DASBOARD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.TextBox txtbox_title;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbox_author;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbox_decription;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_clear;
+    }
+}
+
---
+++ SQLCRUD/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SQLCRUD
+{
+    public partial class Form1 : Form 
+    {
+        string ConnString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\DotNetPro\SQLCRUD\SQLCRUD\book.mdf;Integrated Security=True";
+        int Id;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            viewData();
+            clear();
+            btn_delete.Enabled = false;
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnString))
+            {
+                conn.Open();
+                SqlCommand sqlcmd = new SqlCommand("addEdit", conn);
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+                sqlcmd.Parameters.AddWithValue("@Id", Id);
+                sqlcmd.Parameters.AddWithValue("@Title", txtbox_title.Text.Trim());
+                sqlcmd.Parameters.AddWithValue("@Author", txtbox_author.Text.Trim());
+                sqlcmd.Parameters.AddWithValue("@Description", txtbox_decription.Text.Trim());
+                sqlcmd.ExecuteNonQuery();
+                MessageBox.Show("Data saved successfully");
+                viewData();
+                clear();
+            }
+        }
+
+        void viewData()
+        {
+            using (SqlConnection conn = new SqlConnection(ConnString))
+            {
+                conn.Open();
+                SqlDataAdapter dta = new SqlDataAdapter("bookViewAll", conn);
+                dta.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dtbl = new DataTable();
+                dta.Fill(dtbl);
+                dgv.DataSource = dtbl;
+                dgv.Columns[0].Visible = false;
+                
+            }
+        }
+
+        private void clear()
+        {
+            txtbox_author.Text = txtbox_title.Text = txtbox_decription.Text = "";
+            Id = 0;
+            btn_delete.Enabled = false;
+        }
+
+        private void dgv_DoubleClick(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnString))
+            {
+                conn.Open();
+                if(dgv.CurrentRow.Index != -1)
+                {
+                    txtbox_title.Text = dgv.CurrentRow.Cells[1].Value.ToString();
+                    txtbox_author.Text = dgv.CurrentRow.Cells[2].Value.ToString();
+                    txtbox_decription.Text = dgv.CurrentRow.Cells[3].Value.ToString();
+                    Id = Convert.ToInt32(dgv.CurrentRow.Cells[0].Value.ToString());
+                    btn_save.Text = "Update";
+                    btn_delete.Enabled = true;
+
+                }
+
+            }
+
+        }
+
+      
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("deleteId", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", dgv.CurrentRow.Cells[0].Value);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data deleted successfully");
+                clear();
+                viewData();
+            }
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+    }
+
+}
